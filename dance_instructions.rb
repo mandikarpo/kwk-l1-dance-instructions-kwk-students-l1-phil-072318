73057491912a209f3adaab1b0f3555c820c09771
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-24 13:46:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,8 +1,8 @@
 
 # Code your methods below
 def proper_stance
-puts "plant legs far apart, bend knees slightly and keep posture loose"
+  puts "plant legs far apart, bend knees slightly and keep posture loose"
 def base_footwork
-puts "ligt right foot"
-puts "return right foot to the ground"
-puts
+  puts "ligt right foot"
+  puts "return right foot to the ground"
+  puts "finish with small skip-step backward"
